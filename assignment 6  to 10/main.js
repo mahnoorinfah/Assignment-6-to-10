1e8407bdcbadc0bf0ec92111b0e6c9864ed9b0fd
MahnoorShafiq <161619478+mahnoorinfah@users.noreply.github.com>
2024-05-28 10:59:08 -0700

    typescript codes

---
+++ assignment 6  to 10/main.js
@@ -0,0 +1,49 @@
+//Assignment no 6
+//Shipping Names:Store a person's name, and include some whitespace characters at the beginning and end of
+//name.Make sure you use each character combination. "\t" and "\n" , at least once.Print the name once, so
+//the whitespace around the name is displayed then print the name after striping the whitespaces.
+var namewhite_space = "\t \n mahnoor shafiq \t \n";
+console.log("name with white space", namewhite_space);
+var strippedName = namewhite_space.trim();
+console.log("stripped Name:", strippedName);
+//Assignment no 7
+//Number Eight:Write addition,subtraction,multiplication, and division operations that each result in the 8.
+//Be sure to enclose your operations in print statements to se the results.
+var addition = 5 + 3;
+console.log("Addition result:", addition);
+var subtraction = 10 - 2;
+console.log("Subtaction result:", subtraction);
+var multiplication = 4 * 2;
+console.log("Multiply result:", multiplication);
+var division = 80 / 10;
+console.log("Divide result:", division);
+//Assignment no 8
+//You should create  four lines that look like this:
+//console.log(5 + 3)
+//your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(40 / 5);
+//Assignment no 9
+//Favourite Number:Store your favourite no in a variable. Then,using that variable, create a message that 
+//reveals your favourite number. Print that message.
+var favouriteNumber = 6;
+var number_fav = "My favorite number is: ".concat(favouriteNumber);
+console.log(number_fav);
+//Assignment no 10
+//Adding coments:Choose two of the programs you've written ,and add at least one comment to each, if 
+//you don't have anything specific to write because your programs are too simple at this point, just add 
+//your name and the current date at the top of each program file. then write one sentence describing
+//what the program does.
+//program 1
+//firstly declare two variables and assign values 
+var Number1 = 45;
+var Number2 = 5;
+//now divide those  variables value
+var final_result = Number1 / Number2;
+//at last print the result by using console
+console.log(final_result);
+//program 2
+var myHouse = "very beautiful"; //here I declare a variable
+console.log("my house is:", myHouse); //and here I print that
---
+++ assignment 6  to 10/main.ts
@@ -0,0 +1,75 @@
+//Assignment no 6
+//Shipping Names:Store a person's name, and include some whitespace characters at the beginning and end of
+//name.Make sure you use each character combination. "\t" and "\n" , at least once.Print the name once, so
+//the whitespace around the name is displayed then print the name after striping the whitespaces.
+ let namewhite_space:string = "\t \n mahnoor shafiq \t \n";
+ console.log("name with white space", namewhite_space);     
+ let strippedName:string = namewhite_space.trim();
+ console.log("stripped Name:", strippedName);
+
+ //Assignment no 7
+ //Number Eight:Write addition,subtraction,multiplication, and division operations that each result in the 8.
+ //Be sure to enclose your operations in print statements to se the results.
+
+ let addition:number = 5+3;
+ console.log("Addition result:", addition);
+
+ let subtraction:number = 10-2;
+ console.log("Subtaction result:", subtraction);
+
+ let multiplication:number = 4*2;
+ console.log("Multiply result:", multiplication);
+
+ let division:number = 80/10;
+ console.log("Divide result:", division);
+
+ //Assignment no 8
+ //You should create  four lines that look like this:
+ //console.log(5 + 3)
+ //your output should simply be four lines with the number 8 appearing once on each line.
+ console.log(5 + 3);
+ console.log(10 - 2);
+ console.log(4 * 2);
+ console.log(40 / 5);
+
+ //Assignment no 9
+ //Favourite Number:Store your favourite no in a variable. Then,using that variable, create a message that 
+ //reveals your favourite number. Print that message.
+
+ let favouriteNumber:number = 6;
+ let number_fav:string = `My favorite number is: ${favouriteNumber}`;
+ console.log(number_fav);
+
+ //Assignment no 10
+ //Adding coments:Choose two of the programs you've written ,and add at least one comment to each, if 
+ //you don't have anything specific to write because your programs are too simple at this point, just add 
+ //your name and the current date at the top of each program file. then write one sentence describing
+ //what the program does.
+
+ //program 1
+ //firstly declare two variables and assign values 
+ let Number1:number = 45;
+ let Number2:number = 5;
+ //now divide those  variables value
+ let final_result:number = Number1 / Number2;
+ //at last print the result by using console
+ console.log(final_result);
+
+ //program 2
+
+ let myHouse:string = "very beautiful"; //here I declare a variable
+ 
+ console.log("my house is:", myHouse);//and here I print that
+ 
+ 
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+
